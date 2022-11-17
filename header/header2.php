2f6c2b19882f4e4428fc4e79ffcd7d6f70e74ad5
eed2e <49696693+eed2e@users.noreply.github.com>
2022-11-16 18:56:42 -0600

    .

--- header/header2.php
+++ header/header2.php
@@ -1,3 +1,82 @@
+
+ <!-- ======= Footer ======= -->
+ <footer id="footer" class="footer">
+
+<div class="footer-content">
+  <div class="container">
+    <div class="row">
+
+      <div class="col-lg-4 col-md-6">
+        <div class="footer-info">
+          <h3>DIGITALNET</h3>
+          <p>
+            Gabriela Mistral #818 <br>
+            Santa Anita IV<br>
+            Aguascalientes, Ags<br><br>
+            <strong>Telefono:</strong> 449-972-1609<br>
+            <strong>Email:</strong> contrataciones@digitalnet.com.mx<br>
+        <a target="_blank" href="https://api.whatsapp.com/send/?phone=524499721609&text=Hola disculpe, Necesito mas informacion!"><i class="bi bi-whatsapp"></i> Envíanos un mensaje de WhatsApp</a>
+
+          </p>
+        </div>
+      </div>
+
+      <div class="col-lg-3 col-md-6 footer-links">
+        <h4> Links Importantes</h4>
+        <ul>
+          <li><i class="bi bi-chevron-right"></i> <a href="index.php">inicio</a></li>
+          <li><i class="bi bi-chevron-right"></i> <a href="nosotros.php">Sobre Nosotros</a></li>
+          <li><i class="bi bi-chevron-right"></i> <a href="paquetes.php">Paquetes</a></li>
+          <li><i class="bi bi-chevron-right"></i> <a href="soporte.php">Soporte</a></li>
+          <li><i class="bi bi-chevron-right"></i> <a href="contacto.php">Contacto</a></li>
+          <li></li>
+        </ul>
+      </div>
+
+      <div class="col-lg-1 col-md-6 footer-links">
+       
+        
+      </div>
+
+      <div class="col-lg-4 col-md-6 footer-newsletter">
+        <h4><img src="assets/img/logo_footer.png"></h4>
+        <p>DigitalNet es una empresa especializada en proveer servicios de Internet a localidades donde no llega ninguna otra compañía. Orillas de la ciudad de Aguascalientes (nivel medio bajo - medio alto), comunidades rurales y municipios.</p>
+        
+
+      </div>
+
+    </div>
+  </div>
+</div>
+
+<div class="footer-legal text-center">
+  <div class="container d-flex flex-column flex-lg-row justify-content-center justify-content-lg-between align-items-center">
+
+    <div class="d-flex flex-column align-items-center align-items-lg-start">
+      <div class="copyright">
+        &copy; Copyright <strong><span>HeroBiz</span></strong>. All Rights Reserved
+      </div>
+      <div class="credits">
+        <!-- All the links in the footer should remain intact. -->
+        <!-- You can delete the links only if you purchased the pro version. -->
+        <!-- Licensing information: https://bootstrapmade.com/license/ -->
+        <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/herobiz-bootstrap-business-template/ -->
+        Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+      </div>
+    </div>
+
+    <div class="social-links order-first order-lg-last mb-3 mb-lg-0">
+      <a href="#" class="twitter"><i class="bi bi-twitter"></i></a>
+      <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
+      <a href="#" class="instagram"><i class="bi bi-instagram"></i></a>
+      <a href="#" class="google-plus"><i class="bi bi-skype"></i></a>
+      <a href="#" class="linkedin"><i class="bi bi-linkedin"></i></a>
+    </div>
+
+  </div>
+</div>
+
+</footer><!-- End Footer -->
 <a href="#" class="scroll-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 
 <div id="preloader"></div>
